fcd5c3896132e53c8c1ae57b8c7c0b8b7160967e
Fully34 <cfullinw34@gmail.com>
2015-05-27 11:05:48 -0600

    final commit for holdem

--- holdem_draw/holdem.js
+++ holdem_draw/holdem.js
@@ -1,198 +1,199 @@
-// deck DONE
-// - construct
+    // deck - DONE
+    // - construct
 
-// shuffle DONE
-// - random cards
+    // shuffle - DONE
+    // - random cards
 
-// draw
-// - players
-// - community
-// - remove cards from deck
+    // draw - DONE
+    // - players
+    // - community
+    // - remove cards from deck
 
+    // reset - DONE
 
-//===========================================================================//
-//===============================DECK CONTROLS===============================//
+    //=======================================================================//
+    //===========================DECK CONTROLS===============================//
 
-var deck = [];
+    var deck = [];
 
-function deckConstructor() {
+    function deckConstructor() {
 
-    var suits = ["Clubs", "Diamonds", "Hearts", "Spades"]; 
-    var values = ["Ace",2,3,4,5,6,7,8,9,10,"Jack","Queen","King"];
+        var suits = ["Clubs", "Diamonds", "Hearts", "Spades"]; 
+        var values = ["Ace",2,3,4,5,6,7,8,9,10,"Jack","Queen","King"];
 
-    for (var i = 0; i < suits.length; i ++) {
+        for (var i = 0; i < suits.length; i ++) {
 
-        var suit = suits[i];
+            var suit = suits[i];
 
-        for (var x = 0; x < values.length; x ++) {
-            deck.push(" " + values[x] + " of " + suit); 
-        }
-    }
-    return deck;
-};
-
-function clearDeck() {
-
-    deck = [];
-
-}
-
-
-
-function shuffle(arr) {
-
-    var counter = arr.length;
-    var temp = null;
-    var index = null;
-
-    while (counter > 0) {
-
-        index = Math.floor(Math.random()*counter);
-
-        counter --;
-
-        temp = arr[counter];
-        arr[counter] = arr[index];
-        arr[index] = temp;
-
-    }
-
-    return arr;
-
-}
-
-
-//===========================================================================//
-//=============================PLAYER CONTROLS===============================//
-
-//initiate players with empty array as their value
-
-var players = [];
-
-function playerAdd(num) {
-
-    for (var i = 0; i <= num; i ++) {
-    
-            players[i] = [];
-
-        }
-    return players;
-};
-
-function clearPlayer() {
-    players = [];
-}
-
-
-//===========================================================================//
-//===============================DRAW CONTROLS===============================//
-
-// Need to take cards out of 'deck' in the proper order and put them into the object.
-// also need to push cards to community and burned piles.
-
-var community = [];
-var burned = [];
-
-
-function playerDeal() {
-
-    var card = null;
-    var playerLength = null;
-    // players
-    for (var j = 0; j < 2; j ++){
-
-        for (var i = 0; i < players.length; i ++) {
-
-            playerLength = players[i];
-
-            for (var x = 0; x < 1; x ++){
-
-                var card = deck.pop();
-                players[i].push(card);
-
+            for (var x = 0; x < values.length; x ++) {
+                deck.push(" " + values[x] + " of " + suit); 
             }
         }
-    }
-    return deck;
-}
+        return deck;
+    };
 
+    function clearDeck() {
 
-function takeTop(arr) {
+        deck = [];
 
-    var card = arr.pop();
-
-    return card;
-}
-
-function burn() {
-
-    var card = takeTop(deck);
-    burned.push(card);
-
-    return deck;
-
-}
-
-function flop() {
-
-    var flop = [];
-
-    for (var i = 0; i < 3; i ++) {
-        var card = takeTop(deck);
-        flop.push(card);
     }
 
-    community.push(flop);
 
-    return deck;
-}
 
-function turnRiver() {
+    function shuffle(arr) {
 
-    var card = takeTop(deck);
-    community.push(card);
+        var counter = arr.length;
+        var temp = null;
+        var index = null;
 
-    return deck;
-}
+        while (counter > 0) {
 
-function fullDeal() {
+            index = Math.floor(Math.random()*counter);
 
-    playerDeal();
-    burn();
-    flop();
-    burn();
-    turnRiver(); // --> TURN
-    burn();
-    turnRiver(); // --> RIVER
+            counter --;
 
-    for (var i = 0; i < players.length; i ++) {
-        if (i === 0) {
-            console.log("Your hand: " + players[i]);
-        } else {
-            console.log("CPU " + i + " hand: " + players[i]);
+            temp = arr[counter];
+            arr[counter] = arr[index];
+            arr[index] = temp;
+
         }
+
+        return arr;
+
     }
 
-    console.log("Flop: " + community[0]);
-    console.log("Turn: " + community[1]);
-    console.log("River: " + community[2]);
-}
-//===========================================================================//
-//===============================  BIG RESET  ===============================//
 
-function reset() {
-    
-    var input = prompt("How many computers would you like to add?  (between 1 and 6!)")
+    //=======================================================================//
+    //=========================PLAYER CONTROLS===============================//
 
-    clearPlayer();
-    clearDeck();
-    burned = [];
-    community = [];
+    //initiate players with empty array as their value
 
-    deckConstructor();
-    shuffle(deck);
+    var players = [];
 
-    playerAdd(input);
+    function playerAdd(num) {
 
-    return deck;
-}
+        for (var i = 0; i <= num; i ++) {
+        
+                players[i] = [];
+
+            }
+        return players;
+    };
+
+    function clearPlayer() {
+        players = [];
+    }
+
+
+    //=======================================================================//
+    //===========================DRAW CONTROLS===============================//
+
+    // Need to take cards out of 'deck' in the proper order and put them into the object.
+    // also need to push cards to community and burned piles.
+
+    var community = [];
+    var burned = [];
+
+
+    function playerDeal() {
+
+        var card = null;
+        var playerLength = null;
+        // players
+        for (var j = 0; j < 2; j ++){
+
+            for (var i = 0; i < players.length; i ++) {
+
+                playerLength = players[i];
+
+                for (var x = 0; x < 1; x ++){
+
+                    var card = deck.pop();
+                    players[i].push(card);
+
+                }
+            }
+        }
+        return deck;
+    }
+
+
+    function takeTop(arr) {
+
+        var card = arr.pop();
+
+        return card;
+    }
+
+    function burn() {
+
+        var card = takeTop(deck);
+        burned.push(card);
+
+        return deck;
+
+    }
+
+    function flop() {
+
+        var flop = [];
+
+        for (var i = 0; i < 3; i ++) {
+            var card = takeTop(deck);
+            flop.push(card);
+        }
+
+        community.push(flop);
+
+        return deck;
+    }
+
+    function turnRiver() {
+
+        var card = takeTop(deck);
+        community.push(card);
+
+        return deck;
+    }
+
+    function fullDeal() {
+
+        playerDeal();
+        burn();
+        flop();
+        burn();
+        turnRiver(); // --> TURN
+        burn();
+        turnRiver(); // --> RIVER
+
+        for (var i = 0; i < players.length; i ++) {
+            if (i === 0) {
+                console.log("Your hand: " + players[i]);
+            } else {
+                console.log("CPU " + i + " hand: " + players[i]);
+            }
+        }
+
+        console.log("Flop: " + community[0]);
+        console.log("Turn: " + community[1]);
+        console.log("River: " + community[2]);
+    }
+    //=======================================================================//
+    //===========================  BIG RESET  ===============================//
+
+    function reset() {
+        
+        var input = prompt("How many computers would you like to add?  (between 1 and 7!)")
+
+        clearPlayer();
+        clearDeck();
+        burned = [];
+        community = [];
+
+        deckConstructor();
+        shuffle(deck);
+
+        playerAdd(input);
+
+        return deck;
+    }
