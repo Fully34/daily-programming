2cb7bb094b1ca7e7adc26b098b6cec045e7141a0
Fully34 <cfullinw34@gmail.com>
2015-06-25 16:50:03 -0600

    working on mangling some sentences

--- easy_to_do_list/script.js
+++ easy_to_do_list/script.js
@@ -1,153 +1,153 @@
-"use strict";
-// Description
+    "use strict";
+    // Description
 
-// Todays challenge will be something slightly different! Atleast I think the challenge is meant to be for today? Wait, am I meant to even be submitting today?
-// Okay maybe I need some help on organising my thoughts before I submit my challenge. A to-do list would be fine, just something so that I can organise my thoughts!
-// It should have the following basic functionality
-// Add an item to the to-do list
-// Delete a selected item from the to-do list
-// And obviously, print out the list so I can see what to do!
-// Formal Inputs & Outputs
+    // Todays challenge will be something slightly different! Atleast I think the challenge is meant to be for today? Wait, am I meant to even be submitting today?
+    // Okay maybe I need some help on organising my thoughts before I submit my challenge. A to-do list would be fine, just something so that I can organise my thoughts!
+    // It should have the following basic functionality
+    // Add an item to the to-do list
+    // Delete a selected item from the to-do list
+    // And obviously, print out the list so I can see what to do!
+    // Formal Inputs & Outputs
 
-// Output description
+    // Output description
 
-// Any output that is created should be user-friendly. When I'm viewing my to-do list, I should be able to easily discern one list item from another.
-// Examples
+    // Any output that is created should be user-friendly. When I'm viewing my to-do list, I should be able to easily discern one list item from another.
+    // Examples
 
-// Input:
-// addItem('Take a shower');
-// addItem('Go to work');
-// viewList();
-// Output:
-// Take a shower
-// Go to work
-// Further Input:
-// addItem('Buy a new phone');
-// deleteItem('Go to work');
-// viewList();
-// Outputs:
-// Take a shower
-// Buy a new phone
-// Finally
-
-// Have a good challenge idea?
-// Consider submitting it to /r/dailyprogrammer_ideas
-
-
-// Functions to call:
-    // addItem();
-    // deleteItem();
+    // Input:
+    // addItem('Take a shower');
+    // addItem('Go to work');
     // viewList();
-    // importantAdd();
+    // Output:
+    // Take a shower
+    // Go to work
+    // Further Input:
+    // addItem('Buy a new phone');
+    // deleteItem('Go to work');
+    // viewList();
+    // Outputs:
+    // Take a shower
+    // Buy a new phone
+    // Finally
 
-var toDo = ["1) hello", "2) goodbye", "3) greetings"];
+    // Have a good challenge idea?
+    // Consider submitting it to /r/dailyprogrammer_ideas
 
-//=============================== ADDING ITEM ==============================//
 
-function addItem() {
+    // Functions to call:
+        // addItem();
+        // deleteItem();
+        // viewList();
+        // importantAdd();
 
-    var item = prompt("What would you like to add to your to-do list?");
+    var toDo = ["1) hello", "2) goodbye", "3) greetings"];
 
-    toDo.push(item.toLowerCase());
+    //=============================== ADDING ITEM ==============================//
 
-    // PLAYING WITH HOW TO GET THE TO DO LIST NUMBERED
+    function addItem() {
 
-    for (var i = toDo.length - 1; i < toDo.length; i ++) {
+        var item = prompt("What would you like to add to your to-do list?");
 
-        toDo[i] = (i + 1) + ") " + toDo[i];
-    }
+        toDo.push(item.toLowerCase());
 
-    return viewList();
-}
-        
+        // PLAYING WITH HOW TO GET THE TO DO LIST NUMBERED
 
-//============================ ADD IMPORTANT ===============================//
-
-// Need to place new item at front and re-number all the subsequent items
-
-function importantAdd () {
-
-    var item = prompt (" what would you like to add to the top of your list?");
-
-    toDo.unshift(item.toLowerCase());
-
-    for (var i = 0; i < toDo.length; i ++) {
-
-        // Normal addItem(); function for the new first item
-
-        if (i === 0) {
-        
-            toDo[i] = (i + 1) + ") " + toDo[i];
-
-        } else { // RE-NUMBER SUBSEQUENT ITEMS BY GETTING RID OF THE "#) " and replacing it with the new (i + 1) value.
-
-            toDo[i] = (toDo[i].slice(3, toDo[i].length));
+        for (var i = toDo.length - 1; i < toDo.length; i ++) {
 
             toDo[i] = (i + 1) + ") " + toDo[i];
         }
+
+        return viewList();
     }
-    return toDo;
-}
+            
 
-//=============================  DELETE ITEM  =============================//
+    //============================ ADD IMPORTANT ===============================//
 
+    // Need to place new item at front and re-number all the subsequent items
 
-function deleteItem() {
+    function importantAdd () {
 
-    var item = prompt("What have you completed?");
+        var item = prompt (" what would you like to add to the top of your list?");
 
-    if (typeof(checkList(item)) !==  "string") {
-
-        toDo.splice(checkList(item), 1);
-
-// //EASILY COULD BE MORE MODULAR HERE: SAME LOGIC AS IN BOTH ADD FUNCTIONS, too lazy to abstract right now
+        toDo.unshift(item.toLowerCase());
 
         for (var i = 0; i < toDo.length; i ++) {
 
-            toDo[i] = toDo[i] = (toDo[i].slice(3, toDo[i].length));
+            // Normal addItem(); function for the new first item
 
-            toDo[i] = (i + 1) + ") " + toDo[i];
+            if (i === 0) {
+            
+                toDo[i] = (i + 1) + ") " + toDo[i];
+
+            } else { // RE-NUMBER SUBSEQUENT ITEMS BY GETTING RID OF THE "#) " and replacing it with the new (i + 1) value.
+
+                toDo[i] = (toDo[i].slice(3, toDo[i].length));
+
+                toDo[i] = (i + 1) + ") " + toDo[i];
+            }
         }
-
         return toDo;
     }
 
-    return "That's not on your to-do list!"
-}
+    //=============================  DELETE ITEM  =============================//
 
 
-//==============================  VIEW LIST  ================================//
+    function deleteItem() {
 
-function viewList() {
+        var item = prompt("What have you completed?");
 
-    if (toDo.length < 1 ) {
+        if (typeof(checkList(item)) !==  "string") {
 
-        return "You don't have anything on your to-do list!";
+            toDo.splice(checkList(item), 1);
 
-    } else {
-    
+    // //EASILY COULD BE MORE MODULAR HERE: SAME LOGIC AS IN BOTH ADD FUNCTIONS, too lazy to abstract right now
+
+            for (var i = 0; i < toDo.length; i ++) {
+
+                toDo[i] = toDo[i] = (toDo[i].slice(3, toDo[i].length));
+
+                toDo[i] = (i + 1) + ") " + toDo[i];
+            }
+
+            return toDo;
+        }
+
+        return "That's not on your to-do list!"
+    }
+
+
+    //==============================  VIEW LIST  ================================//
+
+    function viewList() {
+
+        if (toDo.length < 1 ) {
+
+            return "You don't have anything on your to-do list!";
+
+        } else {
+        
+            for (var i = 0; i < toDo.length; i ++) {
+
+                console.log(toDo[i]);
+            }
+        }
+    }
+
+
+    //=============================  CHECK LIST  ===============================//
+
+    // checking current toDo array to see if an item is there.  
+    // This works nicely with the numbered return that addItem(); creates.
+    // NEED TO RETURN THE INDEX OF THE ITEM IF IT'S IN THE ARRAY SO THAT WE CAN DELETE IT.  
+    function checkList(item) {
+
         for (var i = 0; i < toDo.length; i ++) {
 
-            console.log(toDo[i]);
+            if(item === (toDo[i].slice(3, toDo[i].length))) {
+
+                return i;
+            }
         }
+        return "Not here";
     }
-}
-
-
-//=============================  CHECK LIST  ===============================//
-
-// checking current toDo array to see if an item is there.  
-// This works nicely with the numbered return that addItem(); creates.
-// NEED TO RETURN THE INDEX OF THE ITEM IF IT'S IN THE ARRAY SO THAT WE CAN DELETE IT.  
-function checkList(item) {
-
-    for (var i = 0; i < toDo.length; i ++) {
-
-        if(item === (toDo[i].slice(3, toDo[i].length))) {
-
-            return i;
-        }
-    }
-    return "Not here";
-}
 
---
+++ mangle-sentences/script.js
@@ -0,0 +1,113 @@
+// [2015-06-22] Challenge #220 [Easy] Mangling sentences
+// submitted 3 days ago * by XenophonOfAthens2 1
+// Description
+
+// In this challenge, we are going to take a sentence and mangle it up by sorting the letters in each word. So, for instance, if you take the word "hello" and sort the letters in it, you get "ehllo". If you take the two words "hello world", and sort the letters in each word, you get "ehllo dlorw".
+// Inputs & outputs
+
+// Input
+
+// The input will be a single line that is exactly one English sentence, starting with a capital letter and ending with a period
+// Output
+
+// The output will be the same sentence with all the letters in each word sorted. Words that were capitalized in the input needs to be capitalized properly in the output, and any punctuation should remain at the same place as it started. So, for instance, "Dailyprogrammer" should become "Aadegilmmoprrry" (note the capital A), and "doesn't" should become "denos't".
+// To be clear, only spaces separate words, not any other kind of punctuation. So "time-worn" should be transformed into "eimn-ortw", not "eimt-norw", and "Mickey's" should be transformed into "Ceikms'y", not anything else.
+// Edit: It has been pointed out to me that this criterion might make the problem a bit too difficult for [easy] difficulty. If you find this version too challenging, you can consider every non-alphabetic character as splitting a word. So "time-worn" becomes "eimt-norw" and "Mickey's" becomes ""Ceikmy's". Consider the harder version as a Bonus.
+// Sample inputs & outputs
+
+// Input 1
+
+// This challenge doesn't seem so hard.
+
+// Output 1
+
+// Hist aceeghlln denos't eems os adhr.
+
+// Input 2
+
+// There are more things between heaven and earth, Horatio, than are dreamt of in your philosophy. 
+
+// Output 2
+
+// Eehrt aer emor ghinst beeentw aeehnv adn aehrt, Ahioort, ahnt aer ademrt fo in oruy hhilooppsy.
+// Challenge inputs
+
+// Input 1
+
+// Eye of Newt, and Toe of Frog, Wool of Bat, and Tongue of Dog.
+
+// Input 2
+
+// Adder's fork, and Blind-worm's sting, Lizard's leg, and Howlet's wing. 
+
+// Input 3
+
+// For a charm of powerful trouble, like a hell-broth boil and bubble.
+
+
+//===========================================================================//
+                        /* ~~~ NEW TITLE ~~~ */ 
+//===========================================================================//
+
+var input1 = 'This challenge doesn\'t seem so hard.'
+
+function mangle(string) {
+
+    var stringArr = strToArr(string);
+    var splitArr = letterArr(stringArr) 
+    debugger;
+    for (var i = 0; i < splitArr.length; i ++) {
+
+        for (var j = 0; j<splitArr[i].length; j ++) {
+            
+            //Check to see if a letter is capitalized -> if it is, make it lower case
+
+            if ( splitArr[i][j] === splitArr[i][j].toUpperCase() ) {
+
+                splitArr[i][j] = splitArr[i][j].toLowerCase();
+                console.log(splitArr);
+            } 
+
+
+        }
+            //need to join here or commas will be preserved
+            splitArr[i] = splitArr[i].join('');
+
+        // This sorts, but puts special characters to the front
+        // look into regex to check if something is a special character and if so, keep it where it is.  
+
+        splitArr[i] = splitArr[i].sort();
+
+        // Need to have function to return indeces that were capitalized and re-capitalize after sort
+
+    }
+    return splitArr.join(' ');
+};
+
+
+function strToArr(string) {
+
+    return string.split(' ');
+}
+
+function letterArr(arr) {
+
+    for (var i = 0; i < arr.length; i ++) {
+
+        arr[i] = arr[i].split('');
+        console.log(arr[i]);
+    }
+
+    return arr;
+}
+
+
+
+
+
+
+
+
+
+
+
